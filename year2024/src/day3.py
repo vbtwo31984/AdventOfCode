2712d7f2d87b1e58e371fec749e4bc6de9ea34f2
Vladimir Burmistrovich <vbtwo31984@gmail.com>
2024-12-03 10:45:45 -0500

    day 3 done

--- year2024/src/day3.py
+++ year2024/src/day3.py
@@ -1,9 +1,15 @@
+import re
 class Day3:
     def __init__(self, input):
         self.input = input
 
     def part1(self):
-        return 0
+        pattern = r"mul\(([0-9]{1,3}),([0-9]{1,3})\)"
+        matches = re.findall(pattern, self.input, re.DOTALL)
+        total = sum(int(match[0]) * int(match[1]) for match in matches)
+        return total
     
     def part2(self):
-        return 0
+        do_dont_pattern = r"don\'t\(\).*?do\(\)"
+        self.input = re.sub(do_dont_pattern, "", self.input, flags=re.DOTALL)
+        return self.part1()
--- year2024/tst/test_day3.py
+++ year2024/tst/test_day3.py
@@ -6,16 +6,19 @@ from src.day3 import Day3
 class TestDay3(unittest.TestCase):
 
     def setUp(self):
-        self.example_input = "example input"
-        self.day3_instance = Day3(self.example_input)
+        example_input1 = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+        self.day3_instance = Day3(example_input1)
+
+        example_input2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+        self.day3_instance2 = Day3(example_input2)
 
     def test_part1(self):
         result = self.day3_instance.part1()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 161)
 
     def test_part2(self):
-        result = self.day3_instance.part2()
-        self.assertEqual(result, 0)
+        result = self.day3_instance2.part2()
+        self.assertEqual(result, 48)
 
 if __name__ == '__main__':
     unittest.main()
