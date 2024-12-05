0648fddfc16e05d34103b3475b892eefb284b769
Vladimir Burmistrovich <vladimir@burmistrovich.com>
2024-12-05 10:11:34 -0500

    finish day 5

--- year2024/src/day5.py
+++ year2024/src/day5.py
@@ -1,9 +1,59 @@
+import functools
+
 class Day5:
     def __init__(self, input):
         self.input = input
 
+    def get_input(self):
+        split = self.input.split("\n\n")
+        rules = split[0].splitlines()
+        rules = [rule.split("|") for rule in rules]
+
+        before = {}
+        after = {}
+        for rule in rules:
+            if rule[0] not in after:
+                after[rule[0]] = []
+            if rule[0] not in before:
+                before[rule[0]] = []
+            if rule[1] not in after:
+                after[rule[1]] = []
+            if rule[1] not in before:
+                before[rule[1]] = []
+            before[rule[1]].append(rule[0])
+            after[rule[0]].append(rule[1])
+
+        updates = split[1].splitlines()
+        updates = [update.split(",") for update in updates]
+
+        rules = {"before": before, "after": after}
+        return rules, updates
+    
+    def sort_update(self, rules, update):
+        key_func = functools.cmp_to_key(lambda x, y: -1 if y in rules["after"][x] else -1 if x in rules["before"][y] else 0)
+
+        sorted_nums = sorted(update, key=key_func)
+        return sorted_nums
+    
+    def is_valid(self, rules, update):
+        seen = set()
+        for u in update:
+            for v in rules["after"][u]:
+                if v in seen:
+                    return False
+            seen.add(u)
+        return True
+    
+    def get_middle(self, update):
+        length = len(update)
+        return int(update[length // 2])
+
     def part1(self):
-        return 0
+        rules, updates = self.get_input()
+        total = sum(self.get_middle(update) if self.is_valid(rules, update) else 0 for update in updates)
+        return total
     
     def part2(self):
-        return 0
+        rules, updates = self.get_input()
+        total = sum(self.get_middle(self.sort_update(rules,update)) if not self.is_valid(rules, update) else 0 for update in updates)
+        return total
--- year2024/tst/test_day5.py
+++ year2024/tst/test_day5.py
@@ -6,16 +6,43 @@ from src.day5 import Day5
 class TestDay5(unittest.TestCase):
 
     def setUp(self):
-        self.example_input = "example input"
+        self.example_input = """47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47"""
         self.day5_instance = Day5(self.example_input)
 
     def test_part1(self):
         result = self.day5_instance.part1()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 143)
 
     def test_part2(self):
         result = self.day5_instance.part2()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 123)
 
 if __name__ == '__main__':
     unittest.main()
