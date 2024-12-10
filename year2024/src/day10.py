365f2eff5e96435e7b11a8ef9b96607f99f8e612
Vladimir Burmistrovich <vbtwo31984@gmail.com>
2024-12-10 10:41:24 -0500

    day10 done

--- year2024/src/day10.py
+++ year2024/src/day10.py
@@ -2,8 +2,53 @@ class Day10:
     def __init__(self, input):
         self.input = input
 
+    def get_input(self):
+        lines = self.input.splitlines()
+        input = [list(map(int, list(line))) for line in lines]
+        return input
+    
+    def score(self, input, x, y, depth):
+        score = set()
+        if depth == 9:
+            score.add((x,y))
+            return score
+        
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        for dir in dirs:
+            dx, dy = dir
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < len(input) and 0 <= ny < len(input[0]) and input[nx][ny] == depth + 1:
+                score = score.union(self.score(input, nx, ny, depth + 1))
+        return score
+    
+    def score2(self, input, x, y, depth):
+        if depth == 9:
+            return 1
+        
+        score = 0
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        for dir in dirs:
+            dx, dy = dir
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < len(input) and 0 <= ny < len(input[0]) and input[nx][ny] == depth + 1:
+                score += self.score2(input, nx, ny, depth + 1)
+        return score
+
     def part1(self):
-        return 0
+        input = self.get_input()
+        total = 0
+        for x in range(len(input)):
+            for y in range(len(input[x])):
+                if input[x][y] == 0:
+                    scoreset = self.score(input, x, y, 0)
+                    total += len(scoreset)
+        return total
     
     def part2(self):
-        return 0
+        input = self.get_input()
+        total = 0
+        for x in range(len(input)):
+            for y in range(len(input[x])):
+                if input[x][y] == 0:
+                    total += self.score2(input, x, y, 0)
+        return total
--- year2024/tst/test_day10.py
+++ year2024/tst/test_day10.py
@@ -6,16 +6,23 @@ from src.day10 import Day10
 class TestDay10(unittest.TestCase):
 
     def setUp(self):
-        self.example_input = "example input"
+        self.example_input = """89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732"""
         self.day10_instance = Day10(self.example_input)
 
     def test_part1(self):
         result = self.day10_instance.part1()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 36)
 
     def test_part2(self):
         result = self.day10_instance.part2()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 81)
 
 if __name__ == '__main__':
     unittest.main()
