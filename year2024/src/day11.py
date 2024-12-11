c1553d5fed879046c6e9347abdc377e6d6720bb3
Vladimir Burmistrovich <vbtwo31984@gmail.com>
2024-12-11 10:44:23 -0500

    finish day11

--- year2024/src/day11.py
+++ year2024/src/day11.py
@@ -2,8 +2,35 @@ class Day11:
     def __init__(self, input):
         self.input = input
 
-    def part1(self):
-        return 0
+    def transform(self, stone):
+        stone_str = str(stone)
+        if stone == 0:
+            return [1]
+        elif len(stone_str) % 2 == 0:
+            return [int(stone_str[:len(stone_str)//2]), int(stone_str[len(stone_str)//2:])]
+        else:
+            return [stone * 2024]
+
+    def part1(self, num_runs = 25):
+        input = list(map(int, self.input.split(" ")))
+        num_map = {}
+        for i in input:
+            if i not in num_map:
+                num_map[i] = 1
+            else:
+                num_map[i] += 1
+        
+        for _ in range(num_runs):
+            new_map = {}
+            for i in num_map:
+                result = self.transform(i)
+                for j in result:
+                    if j not in new_map:
+                        new_map[j] = num_map[i]
+                    else:
+                        new_map[j] += num_map[i]
+            num_map = new_map
+        return sum(new_map.values())
     
     def part2(self):
-        return 0
+        return self.part1(75)
--- year2024/tst/test_day11.py
+++ year2024/tst/test_day11.py
@@ -6,16 +6,12 @@ from src.day11 import Day11
 class TestDay11(unittest.TestCase):
 
     def setUp(self):
-        self.example_input = "example input"
+        self.example_input = "125 17"
         self.day11_instance = Day11(self.example_input)
 
     def test_part1(self):
         result = self.day11_instance.part1()
-        self.assertEqual(result, 0)
-
-    def test_part2(self):
-        result = self.day11_instance.part2()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 55312)
 
 if __name__ == '__main__':
     unittest.main()
