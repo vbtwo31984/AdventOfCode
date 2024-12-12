5f5b49cb670f8bddf6423b20303f8cb0a4a825d3
Vladimir Burmistrovich <vladimir@burmistrovich.com>
2024-12-12 10:44:32 -0500

    day 12 done

--- year2024/src/day12.py
+++ year2024/src/day12.py
@@ -2,8 +2,74 @@ class Day12:
     def __init__(self, input):
         self.input = input
 
+    def get_input(self):
+        return [list(line) for line in self.input.splitlines()]
+    
+    def get_area_perimeter(self, input, seen: set, x, y):
+        area = 0
+        perimeter = 0
+        sides = set()
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+        visit = [(x, y)]
+        seen.add((x, y))
+        while visit:
+            x, y = visit.pop()
+            area += 1
+            plant = input[x][y]
+            for dir in dirs:
+                dx, dy = dir
+                nx, ny = x + dx, y + dy
+                nxs, nys = x + dx / 4, y + dy / 4
+                if 0 <= nx < len(input) and 0 <= ny < len(input[0]) and (nx, ny) not in seen and input[nx][ny] == plant:
+                    seen.add((nx, ny))
+                    visit.append((nx, ny))
+                elif nx < 0 or ny < 0 or nx >= len(input) or ny >= len(input[0]) or input[nx][ny] != plant:
+                    perimeter += 1
+                    sides.add((nxs, nys))
+
+        num_sides = 0
+        while len(sides) > 0:
+            side = sides.pop()
+            num_sides += 1
+            if side[1] % 1 == 0: # horizontal
+                dy = 1
+                while (side[0], side[1] + dy) in sides:
+                    sides.remove((side[0], side[1] + dy))
+                    dy += 1
+                dy = -1
+                while (side[0], side[1] + dy) in sides:
+                    sides.remove((side[0], side[1] + dy))
+                    dy -= 1
+            else: # vertical
+                dx = 1
+                while (side[0] + dx, side[1]) in sides:
+                    sides.remove((side[0] + dx, side[1]))
+                    dx += 1
+                dx = -1
+                while (side[0] + dx, side[1]) in sides:
+                    sides.remove((side[0] + dx, side[1]))
+                    dx -= 1
+        return area, perimeter, num_sides
+
     def part1(self):
-        return 0
+        input = self.get_input()
+        seen = set()
+        price = 0
+        for x in range(len(input)):
+            for y in range(len(input[0])):
+                if (x, y) not in seen:
+                    a, p, s = self.get_area_perimeter(input, seen, x, y)
+                    price += a * p
+        return price
     
     def part2(self):
-        return 0
+        input = self.get_input()
+        seen = set()
+        price = 0
+        for x in range(len(input)):
+            for y in range(len(input[0])):
+                if (x, y) not in seen:
+                    a, p, s = self.get_area_perimeter(input, seen, x, y)
+                    price += a * s
+        return price
--- year2024/tst/test_day12.py
+++ year2024/tst/test_day12.py
@@ -6,16 +6,25 @@ from src.day12 import Day12
 class TestDay12(unittest.TestCase):
 
     def setUp(self):
-        self.example_input = "example input"
+        self.example_input = """RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE"""
         self.day12_instance = Day12(self.example_input)
 
     def test_part1(self):
         result = self.day12_instance.part1()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 1930)
 
     def test_part2(self):
         result = self.day12_instance.part2()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 1206)
 
 if __name__ == '__main__':
     unittest.main()
