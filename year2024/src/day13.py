833be092081cf5a9b11863b51ab2d86bc454db98
Vladimir Burmistrovich <vladimir@burmistrovich.com>
2024-12-14 18:50:42 -0500

    day13 done

--- year2024/src/day13.py
+++ year2024/src/day13.py
@@ -1,9 +1,65 @@
+import re
+
 class Day13:
     def __init__(self, input):
         self.input = input
 
+    def get_input(self):
+        games = self.input.split("\n\n")
+        input = []
+        for game in games:
+            a_reg = r"A: X\+([0-9]+), Y\+([0-9]+)"
+            a_match = re.search(a_reg, game, re.MULTILINE)
+            a = ([int(a_match.group(1)), int(a_match.group(2))])
+
+            b_reg = r"B: X\+([0-9]+), Y\+([0-9]+)"
+            b_match = re.search(b_reg, game, re.MULTILINE)
+            b = ([int(b_match.group(1)), int(b_match.group(2))])
+
+            prize_reg = r"Prize: X=([0-9]+), Y=([0-9]+)"
+            prize_match = re.search(prize_reg, game, re.MULTILINE)
+            prize = ([int(prize_match.group(1)), int(prize_match.group(2))])
+
+            input.append({"a": a, "b": b, "prize": prize})
+        return input
+    
+    def calculate_score(self, num_a, num_b):
+        return num_a*3 + num_b
+
+    def get_score(self, game):
+        a, b, prize = game["a"], game["b"], game["prize"]
+        score = 0
+
+        a1, a2 = a
+        b1, b2 = b
+        n1, n2 = prize
+
+        nb1 = b1 * a2
+        nn1 = n1 * a2
+
+        nb2 = b2 * a1
+        nn2 = n2 * a1
+
+        b3 = nb1 - nb2
+        n3 = nn1 - nn2
+
+        y = n3 / b3
+        x = (n1 - b1 * y) / a1
+
+
+        if x.is_integer() and y.is_integer():
+            score += self.calculate_score(x, y)
+
+        return int(score)
+
     def part1(self):
-        return 0
+        games = self.get_input()
+        total = sum(self.get_score(game) for game in games)
+        return total
     
     def part2(self):
-        return 0
+        games = self.get_input()
+        for game in games:
+            game["prize"] = (game["prize"][0] + 10000000000000, game["prize"][1] + 10000000000000)
+        total = sum(self.get_score(game) for game in games)
+        return total
--- year2024/tst/test_day13.py
+++ year2024/tst/test_day13.py
@@ -6,16 +6,26 @@ from src.day13 import Day13
 class TestDay13(unittest.TestCase):
 
     def setUp(self):
-        self.example_input = "example input"
+        self.example_input = """Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279"""
         self.day13_instance = Day13(self.example_input)
 
     def test_part1(self):
         result = self.day13_instance.part1()
-        self.assertEqual(result, 0)
-
-    def test_part2(self):
-        result = self.day13_instance.part2()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 480)
 
 if __name__ == '__main__':
     unittest.main()
