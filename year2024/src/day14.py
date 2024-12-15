b0c2ce21a8ff96b3c67067e757044f5f882f2390
Vladimir Burmistrovich <vladimir@burmistrovich.com>
2024-12-14 19:47:35 -0500

    day 14 done

--- year2024/src/day14.py
+++ year2024/src/day14.py
@@ -1,9 +1,70 @@
+import re
+
 class Day14:
-    def __init__(self, input):
+    def __init__(self, input, ax = 101, ay = 103):
         self.input = input
+        self.ax = ax
+        self.ay = ay
+
+    def get_input(self):
+        r = r"-?[0-9]+"
+        lines = self.input.splitlines()
+        return [list(map(int, re.findall(r, line))) for line in lines]
+    
+    def get_position(self, init, move, num_moves):
+        x, y = init
+        dx, dy = move[0] * num_moves, move[1] * num_moves
+        return ((x + dx) % self.ax, (y + dy) % self.ay)
 
     def part1(self):
-        return 0
+        mx = self.ax // 2
+        my = self.ay // 2
+
+        robots = self.get_input()
+        positions = [self.get_position((robot[0],robot[1]), (robot[2], robot[3]), 100) for robot in robots]
+        quadrants = [0, 0, 0, 0]
+        for position in positions:
+            if position[0] < mx and position[1] < my:
+                quadrants[0] += 1
+            elif position[0] < mx and position[1] > my:
+                quadrants[1] += 1
+            elif position[0] > mx and position[1] < my:
+                quadrants[2] += 1
+            elif position[0] > mx and position[1] > my:
+                quadrants[3] += 1
+
+        return quadrants[0] * quadrants[1] * quadrants[2] * quadrants[3]
     
+
+    def count_adjacent_positions(self, positions):
+        adjacent_count = 0
+        for position in positions:
+            x, y = position
+            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                if (x + dx, y + dy) in positions:
+                    adjacent_count += 1
+                    break  # no need to check other directions if one is adjacent
+        return adjacent_count
+    
+    def print_matrix(self, positions):
+        for y in range(self.ay):
+            for x in range(self.ax):
+                if (x, y) in positions:
+                    print("#", end="")
+                else:
+                    print(".", end="")
+            print()
+
     def part2(self):
-        return 0
+        robots = self.get_input()
+        max_adjacent_count = 0
+        max_i = 0
+        for i in range(1, 10000):
+            positions = set([self.get_position((robot[0],robot[1]), (robot[2], robot[3]), i) for robot in robots])
+            adjacent_count = self.count_adjacent_positions(positions)
+            if adjacent_count > max_adjacent_count:
+                max_adjacent_count = adjacent_count
+                max_i = i
+            # if i == 8179:
+            #     self.print_matrix(positions)
+        return max_i
--- year2024/tst/test_day14.py
+++ year2024/tst/test_day14.py
@@ -6,16 +6,23 @@ from src.day14 import Day14
 class TestDay14(unittest.TestCase):
 
     def setUp(self):
-        self.example_input = "example input"
-        self.day14_instance = Day14(self.example_input)
+        self.example_input = """p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3"""
+        self.day14_instance = Day14(self.example_input, 11, 7)
 
     def test_part1(self):
         result = self.day14_instance.part1()
-        self.assertEqual(result, 0)
-
-    def test_part2(self):
-        result = self.day14_instance.part2()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 12)
 
 if __name__ == '__main__':
     unittest.main()
