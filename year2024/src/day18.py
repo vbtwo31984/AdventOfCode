336037821057016bc705edb2dc38c3e82560e909
Vladimir Burmistrovich <vbtwo31984@gmail.com>
2024-12-18 11:26:11 -0500

    day18 done

--- year2024/src/day18.py
+++ year2024/src/day18.py
@@ -1,9 +1,49 @@
 class Day18:
-    def __init__(self, input):
+    def __init__(self, input, size = 71):
         self.input = input
+        self.size = size
+        self.bytes = self.get_bytes()
 
-    def part1(self):
-        return 0
+    def get_bytes(self):
+        lines = self.input.splitlines()
+        return [tuple(map(int, line.split(","))) for line in lines]
+
+    def part1(self, num_bytes = 1024):
+        obstacles = set(self.bytes[0:num_bytes])
+        pos = (0, 0)
+        score = {pos: 0}
+        dirs = ((0, 1), (0, -1), (1, 0), (-1, 0))
+
+        to_visit = [pos]
+        while to_visit:
+            pos = to_visit.pop()
+            for dir in dirs:
+                new_pos = (pos[0] + dir[0], pos[1] + dir[1])
+                if new_pos[0] < 0 or new_pos[1] < 0 or new_pos[0] >= self.size or new_pos[1] >= self.size:
+                    continue
+                if new_pos in obstacles:
+                    continue
+                if new_pos in score and score[new_pos] <= score[pos] + 1:
+                    continue
+                score[new_pos] = score[pos] + 1
+                to_visit.append(new_pos)
+
+        return score[(self.size - 1, self.size - 1)]
     
-    def part2(self):
-        return 0
+    def find_error(self, start, end):
+        mid = (start + end) // 2
+        try:
+            self.part1(mid)
+            if start == mid:
+                return start + 1
+            return self.find_error(mid, end)
+        except KeyError:
+            if start == mid:
+                return start
+            return self.find_error(start, mid)
+
+    def part2(self, start_bytes = 1025):
+        start = start_bytes
+        end = len(self.bytes)
+        i = self.find_error(start, end)
+        return ",".join(map(str, self.bytes[i-1]))
--- year2024/tst/test_day18.py
+++ year2024/tst/test_day18.py
@@ -6,16 +6,40 @@ from src.day18 import Day18
 class TestDay18(unittest.TestCase):
 
     def setUp(self):
-        self.example_input = "example input"
-        self.day18_instance = Day18(self.example_input)
+        self.example_input = """5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0"""
+        self.day18_instance = Day18(self.example_input, 7)
 
     def test_part1(self):
-        result = self.day18_instance.part1()
-        self.assertEqual(result, 0)
+        result = self.day18_instance.part1(12)
+        self.assertEqual(result, 22)
 
     def test_part2(self):
-        result = self.day18_instance.part2()
-        self.assertEqual(result, 0)
-
+        result = self.day18_instance.part2(13)
+        self.assertEqual(result, "6,1")
+    
 if __name__ == '__main__':
     unittest.main()
