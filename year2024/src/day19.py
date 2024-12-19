50fc9d51ceb1a92628dbe0c3252d56669c325591
Vladimir Burmistrovich <vbtwo31984@gmail.com>
2024-12-19 12:03:23 -0500

    day19 done

--- year2024/src/day19.py
+++ year2024/src/day19.py
@@ -1,9 +1,30 @@
 class Day19:
     def __init__(self, input):
         self.input = input
+        lines = input.splitlines()
+        self.patterns = sorted(lines[0].split(", "), reverse=True, key=lambda l: len(l))
+        self.designs = lines[2:]
+        self.checked = {}
+
+    def is_valid(self, design):
+        return self.num_valid(design) > 0
+    
+    def num_valid(self, design):
+        if design in self.checked:
+            return self.checked[design]
+        if len(design) == 0:
+            return 1
+        num = 0
+        for pattern in self.patterns:
+            if design.startswith(pattern):
+                num += self.num_valid(design[len(pattern):])
+        self.checked[design] = num
+        return num
 
     def part1(self):
-        return 0
+        num_valid = sum(1 for design in self.designs if self.is_valid(design))
+        return num_valid
     
     def part2(self):
-        return 0
+        num = sum(self.num_valid(design) for design in self.designs)
+        return num
--- year2024/tst/test_day19.py
+++ year2024/tst/test_day19.py
@@ -6,16 +6,25 @@ from src.day19 import Day19
 class TestDay19(unittest.TestCase):
 
     def setUp(self):
-        self.example_input = "example input"
+        self.example_input = """r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb"""
         self.day19_instance = Day19(self.example_input)
 
     def test_part1(self):
         result = self.day19_instance.part1()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 6)
 
     def test_part2(self):
         result = self.day19_instance.part2()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 16)
 
 if __name__ == '__main__':
     unittest.main()
