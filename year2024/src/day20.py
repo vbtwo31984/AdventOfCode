26074608d550fad82ac6f1db176800d218b12e83
Vladimir Burmistrovich <vladimir@burmistrovich.com>
2024-12-20 10:31:35 -0500

    day20 done

--- year2024/src/day20.py
+++ year2024/src/day20.py
@@ -1,9 +1,48 @@
 class Day20:
     def __init__(self, input):
         self.input = input
+        lines = input.splitlines()
+        self.maze = [list(line) for line in lines]
+        self.track = {}
+        self.populate_maze()
 
-    def part1(self):
-        return 0
+    def find_start(self):
+        for i in range(len(self.maze)):
+            for j in range(len(self.maze[i])):
+                if self.maze[i][j] == "S":
+                    return i, j
+                
+    def populate_maze(self):
+        pos = self.find_start()
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        i = 0
+        while self.maze[pos[0]][pos[1]] != "E":
+            self.track[pos] = i
+            i += 1
+            for dir in dirs:
+                new_pos = (pos[0] + dir[0], pos[1] + dir[1])
+                if new_pos not in self.track and self.maze[new_pos[0]][new_pos[1]] != "#":
+                    pos = new_pos
+                    break
+        self.track[pos] = i
+        
+
+    def part1(self, diff = 100):
+        dirs = [(0, 2), (2, 0), (0, -2), (-2, 0)]
+        total = 0
+        for pos in self.track:
+            for dir in dirs:
+                new_pos = (pos[0] + dir[0], pos[1] + dir[1])
+                if new_pos in self.track and self.track[new_pos] - self.track[pos] - 2 >= diff:
+                    total += 1
+        return total
     
-    def part2(self):
-        return 0
+    def part2(self, diff = 100):
+        total = 0
+        for pos in self.track:
+            for pos2 in self.track:
+                len_cheat = abs(pos[0] - pos2[0]) + abs(pos[1] - pos2[1])
+                if 2 <= len_cheat <= 20:
+                    if self.track[pos2] - self.track[pos] - len_cheat >= diff:
+                        total += 1
+        return total
--- year2024/tst/test_day20.py
+++ year2024/tst/test_day20.py
@@ -6,16 +6,30 @@ from src.day20 import Day20
 class TestDay20(unittest.TestCase):
 
     def setUp(self):
-        self.example_input = "example input"
+        self.example_input = """###############
+#...#...#.....#
+#.#.#.#.#.###.#
+#S#...#.#.#...#
+#######.#.#.###
+#######.#.#...#
+#######.#.###.#
+###..E#...#...#
+###.#######.###
+#...###...#...#
+#.#####.#.###.#
+#.#...#.#.#...#
+#.#.#.#.#.#.###
+#...#...#...###
+###############"""
         self.day20_instance = Day20(self.example_input)
 
     def test_part1(self):
-        result = self.day20_instance.part1()
-        self.assertEqual(result, 0)
+        result = self.day20_instance.part1(40)
+        self.assertEqual(result, 2)
 
     def test_part2(self):
-        result = self.day20_instance.part2()
-        self.assertEqual(result, 0)
+        result = self.day20_instance.part2(50)
+        self.assertEqual(result, 32+31+29+39+25+23+20+19+12+14+12+22+4+3)
 
 if __name__ == '__main__':
     unittest.main()
