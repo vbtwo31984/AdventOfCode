07eb4e81fc4488c323fc507c562add9560b58ba4
Vladimir Burmistrovich <vladimir@burmistrovich.com>
2024-12-23 09:28:30 -0500

    day 22 part 1

--- year2024/src/day22.py
+++ year2024/src/day22.py
@@ -1,9 +1,40 @@
+import functools
+
+
 class Day22:
     def __init__(self, input):
         self.input = input
 
+    def mix(self, secret, num):
+        return secret ^ num 
+    
+    def prune(self, secret):
+        return secret % 16777216
+    
+    def get_next_secret(self, secret):
+        result = secret * 64
+        secret = self.mix(secret, result)
+        secret = self.prune(secret)
+
+        result = secret // 32
+        secret = self.mix(secret, result)
+        secret = self.prune(secret)
+
+        result = secret * 2048
+        secret = self.mix(secret, result)
+        secret = self.prune(secret)
+
+        return secret
+    
     def part1(self):
-        return 0
+        sum = 0
+        lines = self.input.splitlines()
+        for line in lines:
+            secret = int(line)
+            for _ in range(2000):
+                secret = self.get_next_secret(secret)
+            sum += secret
+        return sum
     
     def part2(self):
         return 0
--- year2024/tst/test_day22.py
+++ year2024/tst/test_day22.py
@@ -6,12 +6,26 @@ from src.day22 import Day22
 class TestDay22(unittest.TestCase):
 
     def setUp(self):
-        self.example_input = "example input"
+        self.example_input = """1
+2
+3
+2024"""
         self.day22_instance = Day22(self.example_input)
+
+    def test_mix(self):
+        secret = 42
+        num = 15
+        result = self.day22_instance.mix(secret, num)
+        self.assertEqual(result, 37)
+
+    def test_prune(self):
+        secret = 100000000
+        result = self.day22_instance.prune(secret)
+        self.assertEqual(result, 16113920)
 
     def test_part1(self):
         result = self.day22_instance.part1()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 37990510)
 
     def test_part2(self):
         result = self.day22_instance.part2()
