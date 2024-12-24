c345d6429cbdb4c6d854548217f0d06ddc5adba2
Vladimir Burmistrovich <vladimir@burmistrovich.com>
2024-12-24 09:42:58 -0500

    day 23 part 1

--- year2024/src/day23.py
+++ year2024/src/day23.py
@@ -1,9 +1,33 @@
+from collections import defaultdict
+
+
 class Day23:
     def __init__(self, input):
         self.input = input
 
-    def part1(self):
-        return 0
+    def get_connections(self):
+        lines = self.input.splitlines()
+        connections = defaultdict(set)
+        for line in lines:
+            a, b = line.split("-")
+            connections[a].add(b)
+            connections[b].add(a)
+        return connections
     
+    def get_triplets(self, connections):
+        triplets = set()
+        for node in connections:
+            for next_node in connections[node]:
+                for next_next_node in connections[next_node]:
+                    if node in connections[next_next_node]:
+                        triplets.add(tuple(sorted((node, next_node, next_next_node))))
+        return triplets
+
+    def part1(self):
+        connections = self.get_connections()
+        triplets = self.get_triplets(connections)
+        triplets_starting_with_t = {triplet for triplet in triplets if any(node.startswith("t") for node in triplet)}
+        return len(triplets_starting_with_t)
+            
     def part2(self):
         return 0
--- year2024/tst/test_day23.py
+++ year2024/tst/test_day23.py
@@ -6,12 +6,44 @@ from src.day23 import Day23
 class TestDay23(unittest.TestCase):
 
     def setUp(self):
-        self.example_input = "example input"
+        
+        self.example_input = """kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn"""
         self.day23_instance = Day23(self.example_input)
 
     def test_part1(self):
         result = self.day23_instance.part1()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 7)
 
     def test_part2(self):
         result = self.day23_instance.part2()
