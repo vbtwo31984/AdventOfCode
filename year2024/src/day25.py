4936d137daf965744a67998a631bfa3ed5a61bc4
Vladimir Burmistrovich <vladimir@burmistrovich.com>
2024-12-25 11:01:12 -0500

    day 25

--- year2024/src/day25.py
+++ year2024/src/day25.py
@@ -1,9 +1,41 @@
 class Day25:
     def __init__(self, input):
-        self.input = input
+        parts = input.split("\n\n")
+        locks = []
+        keys = []
+        for part in parts:
+            lines = part.splitlines()
+            if lines[0].startswith("#"):
+                locks.append(self.get_part(lines))
+            else:
+                lines.reverse()
+                keys.append(self.get_part(lines))
+        self.locks = locks
+        self.keys = keys
+
+
+    def get_part(self, lines):
+        key = [0, 0, 0, 0, 0]
+        for i in range(1, len(lines)):
+            line = lines[i]
+            for j in range(len(line)):
+                if line[j] == "." and lines[i-1][j] == "#":
+                    key[j] = i - 1
+        return key
+
+    def is_pair(self, lock, key):
+        for i in range(len(lock)):
+            if lock[i] + key[i] > 5:
+                return False
+        return True
 
     def part1(self):
-        return 0
+        pairs = 0
+        for lock in self.locks:
+            for key in self.keys:
+                if self.is_pair(lock, key):
+                    pairs += 1
+        return pairs
     
     def part2(self):
         return 0
--- year2024/tst/test_day25.py
+++ year2024/tst/test_day25.py
@@ -1,17 +1,55 @@
 import unittest
 import sys
 sys.path.insert(0, 'year2024')
-from src.day25 import Day25
+from day25 import Day25
 
 class TestDay25(unittest.TestCase):
 
     def setUp(self):
-        self.example_input = "example input"
+        self.example_input = """#####
+.####
+.####
+.####
+.#.#.
+.#...
+.....
+
+#####
+##.##
+.#.##
+...##
+...#.
+...#.
+.....
+
+.....
+#....
+#....
+#...#
+#.#.#
+#.###
+#####
+
+.....
+.....
+#.#..
+###..
+###.#
+###.#
+#####
+
+.....
+.....
+.....
+#....
+#.#..
+#.#.#
+#####"""
         self.day25_instance = Day25(self.example_input)
 
     def test_part1(self):
         result = self.day25_instance.part1()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 3)
 
     def test_part2(self):
         result = self.day25_instance.part2()
