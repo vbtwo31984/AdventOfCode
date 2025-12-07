ee9a99b0a3476f9f988af5f9214d3906e9316dbb
Vladimir Burmistrovich <vladimir@burmistrovich.com>
2025-12-07 16:15:17 -0500

    tabulation method for part2

--- src/year_2025/day_07.py
+++ src/year_2025/day_07.py
@@ -53,6 +53,30 @@ class Day07:
             memo[(start, cur_line)] = self.num_timelines(start - 1, cur_line + 1, memo) + self.num_timelines(start + 1, cur_line + 1, memo)
         return memo[(start, cur_line)]
 
+    def num_timelines_tabulation(self, start: int) -> int:
+        num_lines = len(self.data)
+        num_cols = len(self.data[0])
+        
+        # dp[i][j] = number of timelines at line i, column j
+        # Initialize with base case: last row (line num_lines) is all 1s
+        dp = [[0] * num_cols for _ in range(num_lines + 1)]
+        dp[num_lines] = [1] * num_cols
+        
+        # Fill from bottom to top (from line num_lines-1 down to line 1)
+        for i in range(num_lines - 1, 0, -1):
+            for j in range(num_cols):
+                if self.data[i][j] == ".":
+                    # Goes straight down
+                    dp[i][j] = dp[i + 1][j]
+                elif self.data[i][j] == "^":
+                    # Splits to left and right
+                    if j > 0:
+                        dp[i][j] += dp[i + 1][j - 1]
+                    if j < num_cols - 1:
+                        dp[i][j] += dp[i + 1][j + 1]
+        
+        return dp[1][start]
+
     def solve_part2(self) -> int:
         """
         Solve part 2: [Description of part 2].
@@ -62,7 +86,8 @@ class Day07:
         """
         start = self.data[0].index("S")
         memo = {}
-        return self.num_timelines(start, 1, memo)
+        # return self.num_timelines(start, 1, memo)
+        return self.num_timelines_tabulation(start)
 
 
 if __name__ == "__main__":
