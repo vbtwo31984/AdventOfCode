55da74561c70ece6ef09e9e5aeb783cb10dcbc25
Vladimir Burmistrovich <vbtwo31984@gmail.com>
2025-12-09 17:19:08 -0500

    day9 no external libraries

--- src/year_2025/day_09.py
+++ src/year_2025/day_09.py
@@ -3,7 +3,6 @@ Advent of Code 2025 - Day 9: [Puzzle Title]
 https://adventofcode.com/2025/day/9
 """
 
-import shapely
 from src.utils.input_reader import InputReader
 
 
@@ -20,7 +19,7 @@ class Day09:
         self.input_reader = InputReader(input_file, year=2025, day=9)
         self.data = self.input_reader.read_lines()
 
-    def calculate_area(self, polygon: shapely.Polygon | None = None) -> list[tuple[int, str, str]]:
+    def calculate_area(self) -> list[tuple[int, str, str]]:
         areas = []
         calculated = set()
         for p1 in self.data:
@@ -33,8 +32,7 @@ class Day09:
                     continue
                 p2Coords = [int(c) for c in p2.split(",")]
                 area = (abs(p2Coords[0] - p1Coords[0]) + 1) * (abs(p2Coords[1] - p1Coords[1]) + 1)
-                if polygon is None or polygon.contains(shapely.box(p1Coords[0], p1Coords[1], p2Coords[0], p2Coords[1])):
-                    areas.append((area, p1, p2))
+                areas.append((area, p1, p2))
                 calculated.add(sortedPoints)
         return areas
     
@@ -48,6 +46,27 @@ class Day09:
         areas = sorted(self.calculate_area(), reverse=True)
         return areas[0][0]
     
+    def intersects(self, p1: list[int], p2: list[int], polygon: list[tuple[int, int]]) -> bool:
+        minX = min(p1[0], p2[0])
+        maxX = max(p1[0], p2[0])
+        minY = min(p1[1], p2[1])
+        maxY = max(p1[1], p2[1])
+
+        start = polygon[0]
+        for line in polygon[1:]:
+            if line[0] != start[0]: # horizontal line
+                minLine = min(start[0], line[0]) 
+                maxLine = max(start[0], line[0])
+                if minY <= line[1] <= maxY and (minX < minLine < maxX or minX < maxLine < maxX or (minLine < minX and maxLine > maxX)):
+                    return True
+            else: # vertical line
+                minLine = min(start[1], line[1])
+                maxLine = max(start[1], line[1])
+                if minX <= line[0] <= maxX and (minY < minLine < maxY or minY < maxLine < maxY or (minLine < minY and maxLine > maxY)):
+                    return True
+            start = line
+        return False
+    
     def solve_part2(self) -> int:
         """
         Solve part 2: Find the largest rectangle with corners at polygon vertices
@@ -56,16 +75,19 @@ class Day09:
         Returns:
             Area of the largest rectangle
         """
-        
         points: list[tuple[int, int]] = []
         for line in self.data:
             coords = line.split(",")
             points.append((int(coords[0]), int(coords[1])))
         points.append(points[0])
-        polygon = shapely.Polygon(points)
 
-        areas = sorted(self.calculate_area(polygon), reverse=True)
-        return areas[0][0]
+        areas = sorted(self.calculate_area(), reverse=True)
+        for area in areas:
+            p1 = [int(c) for c in area[1].split(",")]
+            p2 = [int(c) for c in area[2].split(",")]
+            if not self.intersects(p1, p2, points):
+                return area[0]
+        return 0
 
 
 if __name__ == "__main__":
